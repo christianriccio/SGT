815d8050aeb2179d82ff5a698082a3142dabe1e4
christianriccio <riccio.christian.21@gmail.com>
2022-02-08 10:21:21 +0100

    Iniatial commit

---
+++ sgt/sgt/sgtApp.swift
@@ -0,0 +1,17 @@
+//
+//  sgtApp.swift
+//  sgt
+//
+//  Created by Christian Riccio on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct sgtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
